8a4441c797ec21b7eb9faf19679f5caa72de9bfd
Qing201 <Qing201@users.noreply.github.com>
2021-04-05 08:55:40 -0700

    Airspeed sensor noise analysis

---
+++ RC Physical Model/Data Analysis/Airspeed sensor noise/airspeed.m
@@ -0,0 +1,27 @@
+MV = mean(airspeed);        %Mean Value Calculation
+
+SD = std(airspeed);         %Standard Deviation Calculation
+
+%Fast Fourier Transform of Data
+FFT = fft(airspeed);
+
+prompt = 'Input Time Duration:';
+t = input(prompt);          %Time Duration of Data
+
+n = numel(airspeed);        %Get number of log data
+Fs = n/t;                   %data frequency
+
+P2 = abs(FFT/t);
+P1 = P2(1:t/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
+f = Fs*(0:(t/2))/t;
+plot(f,P1) 
+title('Noise Frequency')
+xlabel('f (Hz)')
+ylabel('Amplitude')
+
+
+Mean_Value = MV
+
+Standard_Deviation = SD
