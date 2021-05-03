a462642c46fc40c2e252ffc31a329d55ac85476b
Qing201 <Qing201@users.noreply.github.com>
2021-05-03 07:52:12 -0700

    Noise analysis update

--- RC Physical Model/Data Analysis/Noise Analysis/noise analysis.m
+++ RC Physical Model/Data Analysis/Noise Analysis/noise analysis.m
@@ -1,44 +1,52 @@
 n1 = numel(D);        %Get number of log data
-Data = D(1000:n1-1000);
+Fs = n1 / 31015                  %data frequency
+
+
+Data = D(5000*Fs:25000*Fs);
+
+n = numel(Data);        %Get number of log data
+
+%t = n / Fs;
 
 MV = mean(Data);        %Mean Value Calculation
 
 SD = std(Data);         %Standard Deviation Calculation
 
 %Fast Fourier Transform of Data
-FFT = fft(Data);
+%FFT = fft(Data);
 
-prompt = 'Input Time Duration:';
+%prompt = 'Input Time Duration:';
 
 %t = input(prompt);          %Time Duration of Data
 
-n = numel(Data);        %Get number of log data
 
-t=n;
-
-Fs = n/t;                   %data frequency
-
-P2 = abs(FFT/t);
-P1 = P2(1:t/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-
-f = Fs*(0:(t/2))/t;
-
-figure(1)
-plot(f,P1) 
-title('PSD Plot')
-xlabel('f (Hz)')
-ylabel('Amplitude')
+% P2 = abs(FFT/t);
+% P1 = P2(1:t/2+1);
+% P1(2:end-1) = 2*P1(2:end-1);
+% 
+% f = Fs*(0:(t/2))/t;
+% 
+% figure(1)
+% plot(f,P1) 
+% title('PSD Plot')
+% xlabel('f (Hz)')
+% ylabel('Amplitude')
 
 figure(2)
-plot(Data)
-title('Time Zone Plot')
-xlabel('Time?s)')
-ylabel('Air Speed')
+Hs = spectrum.welch;
+psd(Hs,Data,'Fs',Fs)
+axis([-inf,inf,-inf,inf])
+
+figure(3)
+t=linspace(0,20000,n);
+plot(t,Data)
+title('Time Domain Plot')
+xlabel('Time(s)')
+ylabel('Airspeed (m/s)')
 
 
 figure(4)
-histfit(Data)
+histfit(Data,40)
 
 Mean_Value = MV
 
