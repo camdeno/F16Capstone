a0d54086a50afae2496c7af08d1411b70c5fa699
camdeno <72102700+camdeno@users.noreply.github.com>
2021-05-19 13:18:49 -0700

    Handle Flight Log

---
+++ Mathematical Model/System Identification/FlightLogAnalysis.m
@@ -0,0 +1,32 @@
+% Flight Log Analysis
+% Cam Osborn
+
+% Input Flight Log
+filename = 'log_25_2021-5-19-10-32-00.ulg';
+ulog = ulogreader(filename);
+
+% Use Custom Signal Mapping
+FlightObj = flightLogSignalMapping('ulog');
+
+% Check Mapped Signals
+info(FlightObj,"Signal")
+
+% Read Messages
+msg = readTopicMsgs(ulog);
+
+% Set Time interval
+d1 = ulog.StartTime;
+d2 = d1 + duration([0 0 55],'Format','hh:mm:ss.SSSSSS');
+
+% read RC Controller Data
+rcController = readTopicMsgs(ulog,'TopicNames',{'manual_control_setpoint'}, ... 
+'InstanceID',{0},'Time',[d1 d2]); 
+
+% Extract Manual Control Inputs from the joystick / radio
+manualInputs = rcController.TopicMessages{1,1};
+
+pitchControllerData = manualInputs(:,1);
+rollControllerDataData = manualInputs(:,2);
+yawControllerData = manualInputs(:,3);
+
+ 
